7aa1bed5223804e924af7e8d70f766bd32ee28aa
mshuun <106878967+mshuun@users.noreply.github.com>
2023-11-04 21:24:32 +0900

    [Gold II] Title: 중앙값 구하기, Time: 2720 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,12 +1,15 @@
 t = int(input())
-for i in range(t):
+for _ in range(t):
     m = int(input())
-    lst = list()
-    for j in range((m-1)//10+1):
-        lst.extend(list(map(int, input().split())))
+    lst = []
+    for _ in range((m-1)//10+1):
+        lst.extend(map(int, input().split()))
+
     print(m//2+1)
-    l = list()
     for j in range(0, m, 2):
-        l.append(sorted(lst[0:j+1])[j//2])
-    for j in range((m//2)//10+1):
-        print(*l[10*j:10*(j+1)])
+        sorted_list = sorted(lst[:j+1])
+        med = sorted_list[j//2]
+        if j % 20 == 0 and j != 0:
+            print()
+        print(med, end=' ')
+    print()
