97655b446bebcfcd69f5cf08f2438c735906f35a
mshuun <106878967+mshuun@users.noreply.github.com>
2023-11-23 13:32:28 +0900

    [Silver I] Title: 방문, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12968. 방문/방문.py
+++ 백준/Silver/12968. 방문/방문.py
@@ -1,13 +1,2 @@
-def is_possible_to_visit_cells(R, C, K):
-    # Always possible when K = 1
-    if K == 1:
-        return 1
-
-    # For K > 1, check if the total number of cells is even
-    total_cells = R * C
-    if total_cells % 2 == 0:
-        return 1
-    else:
-        return 0
-R,C,K = map(int,input().split())
-print(is_possible_to_visit_cells(R,C,K))
+R,C,K=map(int,input().split())
+print(int(K==1 or R*C%2==0))
