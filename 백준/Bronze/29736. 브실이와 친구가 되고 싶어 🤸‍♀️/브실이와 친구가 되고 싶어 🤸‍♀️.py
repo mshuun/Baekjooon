7f3c11ca9e9bae6aa99219796629481dbfa4d251
mshuun <106878967+mshuun@users.noreply.github.com>
2024-02-18 11:22:36 +0900

    [Bronze IV] Title: 브실이와 친구가 되고 싶어 🤸‍♀️, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.py
+++ 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.py
@@ -1,10 +1,4 @@
-A,B = map(int,input().split())
-K,X = map(int,input().split())
-r=0
-for i in range(10001):
-    if A<=i<=B and K-X<=i<=K+X:
-        r+=1
-if r:
-    print(r)
-else:
-    print('IMPOSSIBLE')
+A,B=map(int,input().split())
+K,X=map(int,input().split())
+r=max(0,min(B,K+X)-max(A,K-X)+1)
+print(r if r else'IMPOSSIBLE')
