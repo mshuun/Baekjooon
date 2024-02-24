f249880ebb85d649d5e9916d3f0f3ff664036612
mshuun <106878967+mshuun@users.noreply.github.com>
2024-02-24 11:30:20 +0900

    [Bronze III] Title: 파인만, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5724. 파인만/파인만.py
+++ 백준/Bronze/5724. 파인만/파인만.py
@@ -1,8 +1,4 @@
 while True:
     n = int(input())
-    if n == 0:
-        break
-    sq = 0
-    for i in range(1, n+1):
-        sq += (n - (i - 1))**2
-    print(sq)
+    if n==0:break
+    print(sum([(n-i+1)**2 for i in range(1,n+1)]))
