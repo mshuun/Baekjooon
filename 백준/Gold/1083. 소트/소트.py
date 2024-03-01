e1c8d3aeaeafd14cfba86e48ff9f32fdccac43a5
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-01 18:04:15 +0900

    [Gold V] Title: 소트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -1,22 +1,20 @@
-def maximize_number(e, S):
-    n = len(e)
-
-    for i in range(n):
-        max_pos = i
-        for j in range(i + 1, min(i + S + 1, n)):
-            if e[j] > e[max_pos]:
-                max_pos = j
-
-        while max_pos > i:
-            e[max_pos], e[max_pos - 1] = e[max_pos - 1], e[max_pos]
-            max_pos -= 1
-            S -= 1
-            if S == 0:
-                return e
-
-    return e
-
-input()
+n = int(input())
 e = list(map(int, input().split()))
 S = int(input())
-print(*maximize_number(e, S))
+f = 1
+for i in range(n):
+    max_pos = i
+    for j in range(i + 1, min(i + S + 1, n)):
+        if e[j] > e[max_pos]:
+            max_pos = j
+
+    while max_pos > i:
+        e[max_pos], e[max_pos - 1] = e[max_pos - 1], e[max_pos]
+        max_pos -= 1
+        S -= 1
+        if S == 0:
+            f = 0
+            print(*e)
+            break
+if f:
+    print(*e)
