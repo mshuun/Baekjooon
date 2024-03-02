e85cb7780e048a4f29669dd9c8d20b839a700173
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-02 13:34:53 +0900

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.c
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int num1, num2;
-
-    scanf("%d %d", &num1, &num2);
-
-    if (num1 % num2 == 0) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
-
-    return 0;
-}
+main(a,b){scanf("%d %d",&a,&b);puts(a%b?"No":"Yes");}
