27ca470210616009d81ca9c445ef4a2df84c8bec
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-05 15:12:18 +0900

    [Silver V] Title: カードの並び替え, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5599. カードの並び替え/カードの並び替え.py
+++ 백준/Silver/5599. カードの並び替え/カードの並び替え.py
@@ -1,11 +1,6 @@
-n = int(input())
-cards = list(range(1, 2*n + 1))
-for _ in range(int(input())):
-    op = int(input())
-    if op == 0:
-        a = cards[:n]
-        b = cards[n:]
-        cards = [val for pair in zip(a, b) for val in pair]
-    else:
-        cards = cards[op:] + cards[:op]
-print(*cards,sep='\n')
+n=int(input())
+c=list(range(1,n*2+1))
+for _ in[0]*int(input()):
+ o=int(input())
+ c=c[o:]+c[:o]if o else[ j for i in zip(c[:n],c[n:]) for j in i]
+print(*c)
