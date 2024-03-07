af38b45480723b56878cd82bb603c094d599dbc6
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-07 21:57:26 +0900

    [Silver IV] Title: 오버워치 월드컵, Time: 324 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13923. 오버워치 월드컵/오버워치 월드컵.py
+++ 백준/Silver/13923. 오버워치 월드컵/오버워치 월드컵.py
@@ -1,33 +1,24 @@
-while True:
- try:
-  A=int(input())
- except:
-  break
- B=[input() for _ in range(A)]
- C=set()
- D=''
- for E in range(3):
+r=range
+while 1:
+ try:A=int(input())
+ except:break
+ B,C,D=[input() for _ in r(A)],set(),''
+ for E in r(3):
   F=''.join(sorted(B[E]))
   if F in C:
    D=F
-   break
   C.add(F)
  G=H=-1
- for I in range(A):
-  for J in range(A):
+ for I in r(A):
+  for J in r(A):
    K=B[I][J]
-   for L in range(J):
+   for L in r(J):
     if G==-1 and K==B[I][L]:
      G=I
-     for M in range(A):
-      if M!=I and B[M][J]==K:
-       H=J
-       break
-     if H==-1:
-      H=L
-   if K not in D:
-    G,H=I,J
+     for M in r(A):
+      if M!=I and B[M][J]==K:H=J
+     if H==-1:H=L
+   if K not in D:G,H=I,J
  for N in D:
   if N not in B[G]:
    print(G+1,H+1,N)
-   break
