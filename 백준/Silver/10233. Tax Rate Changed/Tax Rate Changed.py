26720ec6c1d8c1607789de0ddcb14f772402c25e
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-14 22:25:30 +0900

    [Silver III] Title: Tax Rate Changed, Time: 120 ms, Memory: 33296 KB -BaekjoonHub

--- 백준/Silver/10233. Tax Rate Changed/Tax Rate Changed.py
+++ 백준/Silver/10233. Tax Rate Changed/Tax Rate Changed.py
@@ -1,15 +1,14 @@
-import sys
-input = sys.stdin.readline
-
-while True:
-    x, y, s = map(int,input().split())
-    if x == 0 and y == 0 and s == 0:
-        break
-    max_after_tax_price = 0
-    for before_tax_price1 in range(1, s):
-        for before_tax_price2 in range(1, s):
-            if before_tax_price1 + before_tax_price2 + (before_tax_price1 * x // 100) + (before_tax_price2 * x // 100) == s:
-                after_tax_price1 = before_tax_price1 + (before_tax_price1 * y // 100)
-                after_tax_price2 = before_tax_price2 + (before_tax_price2 * y // 100)
-                max_after_tax_price = max(max_after_tax_price, after_tax_price1 + after_tax_price2)
-    print(max_after_tax_price)
+A = [[i*(100+t)//100 for t in range(100)]for i in range(1000)]
+while 1:
+ x,y,s=map(int, input().split())
+ if x==0:break
+ B,C=s-1,0
+ for D in range(1, s):
+  if B<D:break
+  E,F=A[D][x],A[B][x]
+  while F+E>s:
+   B-=1
+   if B<D:break
+   F=A[B][x]
+  if B>=D and E+F==s:C=max(C,A[D][y]+A[B][y])
+ print(C)
