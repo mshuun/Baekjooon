a07b5d9b681a881c5104cb310675bd98ed06df49
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-16 15:03:12 +0900

    [Bronze IV] Title: 揃った文字 (Matched Letters), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31616. 揃った文字 （Matched Letters）/揃った文字 （Matched Letters）.c
+++ 백준/Bronze/31616. 揃った文字 （Matched Letters）/揃った文字 （Matched Letters）.c
@@ -1,8 +1,1 @@
-main() {
-    int N;char S[101];scanf("%d",&N);scanf("%s",S);int M=1;
-    for(int i=1;i<N;i++){if(S[i]!=S[0]){M=0;break;}}
-
-    if(M)printf("Yes\n");
-    else printf("No\n");
-    
-}
+main(N){char S[101];scanf("%d%s",&N,S);while(--N&&S[N]==*S);puts(N?"No":"Yes");}
