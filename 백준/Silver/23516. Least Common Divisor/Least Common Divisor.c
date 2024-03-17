b8d5c90870d487a70dee43caae84d86d98e2dfcc
mshuun <106878967+mshuun@users.noreply.github.com>
2024-03-17 11:33:56 +0900

    [Silver IV] Title: Least Common Divisor, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/23516. Least Common Divisor/Least Common Divisor.c
+++ 백준/Silver/23516. Least Common Divisor/Least Common Divisor.c
@@ -1,3 +1,3 @@
-int G(int a,int b){return b?G(b,a%b):a;}
-int R(const char *s, const char *P, int T){int Z=strlen(s);if(Z%T!=0)return 0;for(int i=0;i<Z;i++){if(s[i]!=P[i%T])return 0;}return 1;}
-main(){char S[51],T[51];scanf("%s%s",S,T);int X=strlen(S), Y=strlen(T);int e=G(X, Y);for (int i=1; i <= e; i++){if(e%i==0&&R(S,S,i)&&R(T,S,i)){S[i]='\0';printf("%s",S);return 0;}}printf("No solution");}
+G(int a,int b){return b?G(b,a%b):a;}
+R(char *s,char *P,int T){int Z=strlen(s);if(Z%T!=0)return 0;for(int i=0;i<Z;i++){if(s[i]!=P[i%T])return 0;}return 1;}
+main(X,Y,e,i){char S[51],T[51];scanf("%s%s",S,T);X=strlen(S);Y=strlen(T);e=G(X, Y);for (i=1;i<=e;i++){if(e%i==0&&R(S,S,i)&&R(T,S,i)){S[i]='\0';printf("%s",S);return 0;}}printf("No solution");}
