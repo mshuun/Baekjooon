9300461e9ffc29d3ece952324dba26657e9c80e5
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-03-18 12:06:52 +0900

    [Bronze III] Title: 폰 노이만, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9469. 폰 노이만/폰 노이만.c
@@ -0,0 +1,10 @@
+main(T, i)
+{
+    scanf("%d", &T);
+    for (i = 0; i < T; i++)
+    {
+        double N, D, A, B, F;
+        scanf("%lf%lf%lf%lf%lf", &N, &D, &A, &B, &F);
+        printf("%.0lf %.8lf\n", N, F * D / (A + B));
+    }
+}
