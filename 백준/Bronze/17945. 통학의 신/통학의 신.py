49b504ee5eba2e05c127f355048359bd98fef6b6
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-03-21 18:52:19 +0900

    [Bronze III] Title: 통학의 신, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.py
+++ 백준/Bronze/17945. 통학의 신/통학의 신.py
@@ -1,9 +1,4 @@
 a, b = map(int, input().split())
-x = int(-a+(a**2-b)**0.5)
-y = int(-a-(a**2-b)**0.5)
-if x > y:
-    print(y, x)
-elif y < x:
-    print(x, y)
-else:
-    print(x)
+for i in range(-999, 999):
+    if i * i + 2 * a * i == -b:
+        print(i, end=' ')
