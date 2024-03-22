3d4b846591d558d7f3de2a38328dd9a7ea3917e0
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-03-22 17:54:42 +0900

    [Bronze III] Title: 소금 폭탄, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.py
@@ -0,0 +1,10 @@
+h1,m1,s1 = map(int,input().split(':'))
+h2,m2,s2 = map(int,input().split(':'))
+t1 = h1*3600 + m1*60 + s1
+t2 = h2*3600 + m2*60 + s2
+t = t2 - t1
+if t <= 0:t += 24*3600
+h = t // 3600
+m = t % 3600 // 60
+s = t % 60
+print(f"{h:02d}:{m:02d}:{s:02d}")
