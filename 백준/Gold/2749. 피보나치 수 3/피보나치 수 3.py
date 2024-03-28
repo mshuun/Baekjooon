38b119d4acabc3d6eb118afec90d1b4816fb3a7d
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-03-28 17:55:51 +0900

    [Gold II] Title: 피보나치 수 3, Time: 264 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -1,18 +1,5 @@
-P = 1500000
-M = 1000000
-fibo = [0, 1]
-
-
-def fib(n):
-    if n < len(fibo):
-        return fibo[n]
-
-    else:
-        for i in range(len(fibo), n + 1):
-            fibo.append((fibo[i - 2]+fibo[i - 1]) % M)
-        return fibo[-1]
-
-
-N = int(input())
-
-print(fib(N % P) % P)
+a = 0
+b = 1
+for i in range(int(input()) % 1500000-1):
+    a, b = b, (a+b) % 1000000
+print(b % 1500000)
