61b6405c0ea86f7fe4a82a6467b526bca7a2bfc8
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-03-30 21:11:46 +0900

    [Bronze III] Title: Error Detection, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/5220. Error Detection/Error Detection.c
+++ 백준/Bronze/5220. Error Detection/Error Detection.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
-int countOnes(int num)
-{
-    int count = 0;
-    while (num)
-    {
-        count += num & 1;
-        num >>= 1;
-    }
-    return count;
-}
 
 int main()
 {
-    int T, number, valid;
-    scanf("%d", &T);
-    for (int i = 0; i < T; i++)
+    int T, number, valid, count;
+    for (scanf("%d", &T); T--; puts(valid == count % 2 ? "Valid" : "Corrupt"))
     {
         scanf("%d %d", &number, &valid);
-        if (valid == countOnes(number) % 2)
-        {
-            printf("Valid\n");
-        }
-        else
-        {
-            printf("Corrupt\n");
-        }
+        for (count = 0; number; count += number & 1, number >>= 1)
+            ;
     }
     return 0;
 }
