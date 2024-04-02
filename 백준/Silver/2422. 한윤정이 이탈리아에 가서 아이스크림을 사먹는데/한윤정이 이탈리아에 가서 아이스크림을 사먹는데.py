87b451a4c2a4bf5e35a1b5dd5e191f35acc57194
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-03 01:13:12 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 240 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -1,19 +1,18 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-bad = set()
+bad = [[False] * (N + 1) for _ in range(N + 1)]
 for _ in range(M):
     a, b = map(int, input().split())
-    bad.add((a, b))
-    bad.add((b, a))
+    bad[a][b] = True
+    bad[b][a] = True
 
 result = 0
-for i in range(1, N+1):
-    for j in range(i+1, N+1):
-        if (i, j) in bad:
+for i in range(1, N + 1):
+    for j in range(i + 1, N + 1):
+        if bad[i][j]:
             continue
-        for k in range(j+1, N+1):
-            if (i, k) in bad or (j, k) in bad:
-                continue
-            result += 1
+        for k in range(j + 1, N + 1):
+            if not bad[i][k] and not bad[j][k]:
+                result += 1
 print(result)
