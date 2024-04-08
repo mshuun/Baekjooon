c0bd3767ff7e59e34db6e37ff2c4d968b70637ca
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-08 13:46:18 +0900

    [Silver III] Title: 이상한 아리의 채점, Time: 160 ms, Memory: 41184 KB -BaekjoonHub

--- 백준/Silver/25167. 이상한 아리의 채점/이상한 아리의 채점.py
+++ 백준/Silver/25167. 이상한 아리의 채점/이상한 아리의 채점.py
@@ -1,31 +1,19 @@
-from datetime import datetime
-
-N, M, P = map(int, input().split())
-ps = input().split()
-ss = [input().split() for _ in range(P)]
-sc = {p: 0 for p in ps}
-at = {p: {str(i): [] for i in range(1, N+1)} for p in ps}
-
-for pr, t, n, r in ss:
-    at[n][pr].append((datetime.strptime(t, "%H:%M"), r))
-
-for i in range(1, N+1):
-    ps = []
-    for p, prs in at.items():
-        a = prs[str(i)]
-        if not a: 
-            sc[p] += M + 1
-            continue
-        f = s = None
-        for t, r in a:
-            if r == "wrong" and f is None: f = t
-            elif r == "solve": s = t; break
-        if s is None: 
-            sc[p] += M
-        elif f is None: 
-            sc[p] += M + 1
-        else: 
-            ps.append((s - f, p))
-    for r, (_, p) in enumerate(sorted(ps), 1): sc[p] += r
-
-print('\n'.join(sorted(sc, key=lambda x: (sc[x], x))))
+from datetime import datetime as d
+N,M,P=map(int,input().split());A=input().split();B=[input().split() for _ in range(P)];C={p:0 for p in A};D={p:{str(i):[] for i in range(1,N+1)} for p in A};X=None
+for Z,t,n,r in B:D[n][Z].append((d.strptime(t,"%H:%M"),r))
+for i in range(1,N+1):
+ A=[]
+ for p,Y in D.items():
+  a=Y[str(i)]
+  if not a:
+   C[p]+=M+1
+   continue
+  f=s=X
+  for t,r in a:
+   if r=="wrong" and f is X:f=t
+   elif r=="solve":s=t; break
+  if s is X:C[p]+=M
+  elif f is X:C[p]+=M+1
+  else:A.append((s-f,p))
+ for r,(_,p) in enumerate(sorted(A),1):C[p]+=r
+print(*sorted(C,key=lambda x:(C[x],x)))
