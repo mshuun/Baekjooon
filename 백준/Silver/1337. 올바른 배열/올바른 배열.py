c34f7d1b8383974aeffbceffbd492c2458564340
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-11 11:58:45 +0900

    [Silver IV] Title: 올바른 배열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1337. 올바른 배열/올바른 배열.py
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -4,8 +4,8 @@ l.sort()
 r = 5
 for i in range(N):
     for j in range(i, i+5):
-        a = l[i:j+1][::-1]
+        a = l[i:j+1]
         z = len(a)
-        if sum(a[k]-a[k+1] for k in range(z-1)) < 5:
+        if sum(a[k+1]-a[k] for k in range(z-1)) < 5:
             r = min(r, 5-z)
 print(r)
