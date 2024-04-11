8b04a9e31e5f5ff1e7f5856b97eff3f4c197f088
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-11 21:59:11 +0900

    [Silver IV] Title: 아이폰 9S, Time: 440 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5883. 아이폰 9S/아이폰 9S.py
+++ 백준/Silver/5883. 아이폰 9S/아이폰 9S.py
@@ -1,21 +1,9 @@
-N = int(input())
-B = [int(input()) for _ in range(N)]
-
-
-b = list(set(B))
-r = 0
-for i in range(len(b)):
-    newB = [j for j in B if j != b[i]]
-    m = 0
-    c = 1
-    k = newB[0]
-    for i in range(1, len(newB)):
-        if newB[i] == k:
-            c += 1
-        else:
-            k = newB[i]
-            m = max(m, c)
-            c = 1
-    m = max(m, c)
-    r = max(r, m)
-print(r)
+c=[int(input())for _ in range(int(input()))]
+m=0
+for a in set(c):
+ f=[b for b in c if b!=a]
+ l=0
+ for i in range(len(f)):
+  l=l+1if i==0 or f[i]==f[i-1]else 1
+  m=max(m,l)
+print(m)
