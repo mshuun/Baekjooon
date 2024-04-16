6ea14d4be1a95f8af74e68793ecadd5db08446eb
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-16 15:40:32 +0900

    [Gold II] Title: 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2541. 점/점.py
+++ 백준/Gold/2541. 점/점.py
@@ -1,45 +1,30 @@
-def can_generate(a, b, points):
-    def abs_diff(k):
-        return abs(k)
-
-    d = abs_diff(b - a)
-    if a == b:
-        a, b = 1, 1
-    else:
-        while d % 2 == 0:
-            d //= 2
-        if a < b:
-            a, b = 1, 1 + d
-        else:
-            a, b = 1 + d, 1
-
-    results = []
-    for x, y in points:
-        if (a <= b) != (x <= y):
-            results.append("N")
-        else:
-            dd = abs_diff(y - x)
-            if d * dd == 0:
-                if d == dd:
-                    results.append("Y")
-                else:
-                    results.append("N")
-            else:
-                if dd % d == 0:
-                    results.append("Y")
-                else:
-                    results.append("N")
-    return results
-
-
 a, b = map(int, input().split())
-points = []
+d = abs(b - a)
+if a == b:
+    a, b = 1, 1
+else:
+    while d % 2 == 0:
+        d //= 2
+    if a < b:
+        a, b = 1, 1 + d
+    else:
+        a, b = 1 + d, 1
 while True:
     try:
-        points.append(map(int, input().split()))
+        x, y = map(int, input().split())
+        if (a <= b) != (x <= y):
+            print("N")
+        else:
+            dd = abs(y - x)
+            if d * dd == 0:
+                if d == dd:
+                    print("Y")
+                else:
+                    print("N")
+            else:
+                if dd % d == 0:
+                    print("Y")
+                else:
+                    print("N")
     except EOFError:
         break
-
-results = can_generate(a, b, points)
-for result in results:
-    print(result)
