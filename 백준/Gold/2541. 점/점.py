51ef94454d0a1b35bef8911ba3c3bfbb8391a0d1
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-16 15:50:03 +0900

    [Gold II] Title: 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2541. 점/점.py
+++ 백준/Gold/2541. 점/점.py
@@ -1,21 +1,8 @@
-a, b = map(int, input().split())
-D = abs(b - a)
-if a == b:
-    a, b = 1, 1
-else:
-    while D % 2 == 0:
-        D //= 2
-    a, b = (1, 1 + D) if a < b else (1 + D, 1)
-while True:
-    try:
-        x, y = map(int, input().split())
-        if (a <= b) != (x <= y):
-            print("N")
-        else:
-            d = abs(y - x)
-            if D == 0 or d == 0:
-                print("Y"if D == d else "N")
-            else:
-                print("Y" if d % D == 0 else "N")
-    except EOFError:
-        break
+a,b=map(int,input().split())
+D=abs(b-a)
+if a!=b:
+ while D%2==0:D//=2
+ a,b=(1,1+D)if a<b else(1+D,1)
+while 1:
+ try:x,y=map(int,input().split());d=abs(y-x);print('Y'if(a<=b)==(x<=y)and(D==d or d and D and d%D==0)else'N')
+ except:break
