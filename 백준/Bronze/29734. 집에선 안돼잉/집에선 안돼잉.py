b3b8aa5aa4fbdcab4b3e403197dbc4ac67aa3d7d
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-17 00:32:50 +0900

    [Bronze II] Title: 집에선 안돼잉, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29734. 집에선 안돼잉/집에선 안돼잉.py
+++ 백준/Bronze/29734. 집에선 안돼잉/집에선 안돼잉.py
@@ -1,12 +1,5 @@
-N, M = map(int, input().split())
-T, S = map(int, input().split())
-
-home = N + (N//8 - (N % 8 == 0))*S
-dok = M + T + (M//8 - (M % 8 == 0))*(T+T+S)
-
-if home < dok:
-    print('Zip')
-    print(home)
-else:
-    print('Dok')
-    print(dok)
+N,M=map(int,input().split())
+T,S=map(int,input().split())
+h=N+(N//8-(N%8==0))*S
+d=M+T+(M//8-(M%8==0))*(T+T+S)
+print(('Dok','Zip')[h<d],min(h,d))
