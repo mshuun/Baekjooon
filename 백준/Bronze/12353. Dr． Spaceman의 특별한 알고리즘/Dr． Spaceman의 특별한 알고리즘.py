52d4fe1d99c18194a6322f66a8181c08b5ad51f5
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-17 00:00:24 +0900

    [Bronze II] Title: Dr. Spaceman의 특별한 알고리즘, Time: 552 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/12353. Dr． Spaceman의 특별한 알고리즘/Dr． Spaceman의 특별한 알고리즘.py
+++ 백준/Bronze/12353. Dr． Spaceman의 특별한 알고리즘/Dr． Spaceman의 특별한 알고리즘.py
@@ -1,20 +1,3 @@
 from math import ceil
-
-T = int(input())
-for i in range(T):
-    s, mom, dad = input().split()
-    mom = mom.split("'")
-    mom = int(mom[0])*12 + int(mom[1][:-1])
-    dad = dad.split("'")
-    dad = int(dad[0])*12 + int(dad[1][:-1])
-    result = (dad + mom + (5 if s == 'B' else -5)) / 2
-    a = result - 4
-    b = result + 4
-    a_foot = int(a // 12)
-    a_inch = ceil(a % 12)
-    if a_inch == 12:
-        a_foot += 1
-        a_inch = 0
-    b_foot = int(b // 12)
-    b_inch = int(b % 12)
-    print(f"Case #{i+1}: {a_foot}'{a_inch}\" to {b_foot}'{b_inch}\"")
+for i in range(int(input())):
+ s,m,d=input().split();m=int(m[0])*12+int(m[2:-1]);d=int(d[0])*12+int(d[2:-1]);r=(d+m+(5 if s=='B' else -5))/2;a=r-4;b=r+4;a_f=int(a//12);a_i=ceil(a%12);b_f=int(b//12);b_i=int(b%12);a_f,a_i=(a_f+1,0)if a_i==12 else(a_f,a_i);print(f"Case #{i+1}: {a_f}'{a_i}\" to {b_f}'{b_i}\"")
