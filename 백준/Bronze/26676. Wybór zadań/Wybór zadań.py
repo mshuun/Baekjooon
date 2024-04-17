68689b23027d342f4aeec6bf421b8f6fb178bf61
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-17 11:09:38 +0900

    [Bronze III] Title: Wybór zadań, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/26676. Wybór zadań/Wybór zadań.py
+++ 백준/Bronze/26676. Wybór zadań/Wybór zadań.py
@@ -1,10 +1,7 @@
 def c(p):
-    A = {f"{B}{C}": 0 for B in range(1, 6) for C in 'ABC'}
-    for E in p:
-        if E in A: A[E] += 1
-    for B in range(1, 5):
-        for C in 'ABC':
-            if A[f"{B}{C}"] < 1: return "NIE"
-    return "NIE" if any(A[f"5{C}"] < 2 for C in 'ABC') else "TAK"
+ A={f'{i}{j}':0 for i in range(1,6) for j in'ABC'}
+ for e in p:
+  if e in A: A[e]+=1
+ return 'NIE' if any(A[f'{i}{j}']<(2 if i==5 else 1) for i in range(1,6) for j in'ABC') else 'TAK'
 input()
 print(c(input().split()))
