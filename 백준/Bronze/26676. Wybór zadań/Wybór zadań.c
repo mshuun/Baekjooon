ce73f51c80d6a653253423935580a0678dd8d86b
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2024-04-17 11:13:11 +0900

    [Bronze III] Title: Wybór zadań, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/26676. Wybór zadań/Wybór zadań.c
+++ 백준/Bronze/26676. Wybór zadań/Wybór zadań.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 int main() {
-    int n, c[15] = {0}, i, r, d;
-    scanf("%d", &n);
+    int n, c[15] = {0}, i;
     char p[3];
-    while (n--) {
-        scanf("%s", p);
-        r = (p[0] - '1') * 3;
-        d = p[1] - 'A';
-        c[r + d]++;
-    }
-    for (i = 0; i < 15; i++)
-        if (c[i] < 1 + (i / 3 == 4))
+    for(scanf("%d", &n); n--; c[(p[0] - '1') * 3 + p[1] - 'A']++) scanf("%s", p);
+    for(i = 0; i < 15; i++)
+        if(c[i] < 1 + (i / 3 == 4))
             return !printf("NIE\n");
     printf("TAK\n");
 }
