d904198e2bc5f08080958a49613df6f522118650
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-02-28 04:24:10 +0900

    [Silver II] Title: 나무 자르기, Time: 932 ms, Memory: 150292 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,25 +1,31 @@
 import bisect
 import sys
+
 input = sys.stdin.readline
-N,M = map(int,input().split())
-tree = list(map(int,input().split()))
+N, M = map(int, input().split())
+tree = list(map(int, input().split()))
 tree.sort()
+
+prefix = [0] * (N + 1)
+for i in range(N):
+    prefix[i+1] = prefix[i] + tree[i]
 
 r = 0
 a = 0
-b = 1000000000
+b = tree[-1]
+
 while a <= b:
-    c = (a+b)//2
-    take = 0
+    c = (a + b) // 2
+
     idx = bisect.bisect_right(tree, c)
     
-    for i in tree[idx:]:
-        if i>c:
-            take += i-c
-    if take<M:
+    count = N - idx
+    take = (prefix[N] - prefix[idx]) - (count * c)
+    
+    if take < M:
         b = c - 1
-    elif take>=M:
+    else:
         a = c + 1
         r = c
-    
+        
 print(r)
