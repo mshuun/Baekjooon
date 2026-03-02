ba38d1be4820b2f3e06ac0a609b0e5499da4ad52
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-02 20:41:40 +0900

    [Silver II] Title: 쇠막대기, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,17 +1,14 @@
-s = input()
-t = 0
+s = input().replace("()", "*")
 total = 0
 op = 0
+
 for i in s:
     if i == "(":
         op += 1
-        t = 1
-    elif t == 1:
-        t = 0
-        op -= 1
+    elif i == "*":
         total += op
     else:
-        t = 0
         op -= 1
         total += 1
+
 print(total)
