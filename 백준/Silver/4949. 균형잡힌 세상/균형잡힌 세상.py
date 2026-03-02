967ed6524c1e6a86c86a09cd8ef7ed0e94b83538
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-02 21:35:12 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 68 ms, Memory: 33692 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,39 +1,30 @@
-while 1:
-    s = input()
-    if s == '.':
+import sys
+input = sys.stdin.read
+
+lines = input().splitlines()
+
+for line in lines:
+    if line == '.':
         break
     
-    a = [-1]
-    b = [-1]
-    ll = len(s)
-    r=1
-    try:
-        for i in range(ll):
-            c = s[i]
-            if c == '(':
-                a.append(i)
-            elif c == ')':
-                if a[-1] < b[-1]:
-                    r = 0
-                if a[-1] != -1:
-                    del a[-1]
-                else:
-                    r = 0
-            elif c == '[':
-                b.append(i)
-            elif c == ']':
-                if b[-1] < a[-1]:
-                    r = 0
-                if b[-1] != -1:
-                    del b[-1]
-                else:
-                    r = 0
-    except:
-        r = 1
-
-    if len(a)>1 or len(b)>1:
-        r = 0
-    if r:
+    stack = []
+    result = True
+    
+    for c in line:
+        if c in '([':
+            stack.append(c)
+        elif c == ')':
+            if not stack or stack[-1] != '(':
+                result = False
+                break
+            stack.pop()
+        elif c == ']':
+            if not stack or stack[-1] != '[':
+                result = False
+                break
+            stack.pop()
+    
+    if result and not stack:
         print("yes")
     else:
         print("no")
