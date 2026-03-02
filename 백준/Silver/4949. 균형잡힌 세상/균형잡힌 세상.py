99b8a451782dfa244bdb710616980d8224048216
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-02 21:39:19 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 44 ms, Memory: 33692 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,30 +1,34 @@
 import sys
-input = sys.stdin.read
 
-lines = input().splitlines()
+def solve():
+    lines = sys.stdin.read().splitlines()
+    if not lines:
+        return
+    
+    out = []
+    match = {')': '(', ']': '['}
+    
+    for line in lines:
+        if line == '.':
+            break
+        
+        stack = []
+        is_valid = True
+        
+        for c in line:
+            if c in "([":
+                stack.append(c)
+            elif c in ")]":
+                if not stack or stack.pop() != match[c]:
+                    is_valid = False
+                    break
+        
+        if is_valid and not stack:
+            out.append("yes")
+        else:
+            out.append("no")
+            
+    sys.stdout.write('\n'.join(out) + '\n')
 
-for line in lines:
-    if line == '.':
-        break
-    
-    stack = []
-    result = True
-    
-    for c in line:
-        if c in '([':
-            stack.append(c)
-        elif c == ')':
-            if not stack or stack[-1] != '(':
-                result = False
-                break
-            stack.pop()
-        elif c == ']':
-            if not stack or stack[-1] != '[':
-                result = False
-                break
-            stack.pop()
-    
-    if result and not stack:
-        print("yes")
-    else:
-        print("no")
+if __name__ == '__main__':
+    solve()
