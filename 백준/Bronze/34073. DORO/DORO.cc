6726aa116113e8c541375a6a01c2cda838f50551
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-04 22:02:31 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    while (N--) {
+        string w;
+        cin >> w;
+        cout << w << "DORO ";
+    }
+    return 0;
+}
