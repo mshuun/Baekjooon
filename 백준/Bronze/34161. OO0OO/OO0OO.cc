c80e0a8f8a445bac87bb529efe958860e21807e4
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-05 22:11:03 +0900

    [Bronze IV] Title: OO0OO, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (int i = 0; i < 10000; ++i) {
+        cout << "-1\n";
+    }
+
+    return 0;
+}
