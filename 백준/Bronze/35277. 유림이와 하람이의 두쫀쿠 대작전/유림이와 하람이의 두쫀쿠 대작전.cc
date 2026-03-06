f29e860fef4746ce142aedbbc758bd504b60e767
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-06 15:11:01 +0900

    [Bronze IV] Title: 유림이와 하람이의 두쫀쿠 대작전, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/35277. 유림이와 하람이의 두쫀쿠 대작전/유림이와 하람이의 두쫀쿠 대작전.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+
+    cin >> N;
+    cout << N / 2000;
+    return 0;
+}
