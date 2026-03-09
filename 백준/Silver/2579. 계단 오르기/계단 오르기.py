83c010e1123a198a83f874040e68c3de5597c3f3
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-09 22:11:17 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,18 +1,25 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-score = [int(input()) for _ in range(n)]
-dp = [0 for _ in range(n)]
 
-if n>=3:
-    dp[0] = score[0]
-    dp[1] = score[0] + score[1]
-    dp[2] = max(score[0], score[1]) + score[2]
-
-    for i in range(3,n):
-        dp[i] = max(dp[i-2],dp[i-3]+score[i-1]) + score[i]
-
-    print(dp[-1])
+if n == 1:
+    print(int(input()))
+elif n == 2:
+    print(int(input()) + int(input()))
 else:
-    if n==2:
-        print(score[0]+score[1])
-    else:
-        print(score[0])
+    dp = [0] * 3
+    dp[0] = int(input())
+    step2 = int(input())
+    dp[1] = dp[0] + step2
+    
+    prev_score = int(input())
+    dp[2] = max(dp[0], step2) + prev_score
+    
+    for _ in range(3, n):
+        curr_score = int(input())
+        next_dp = max(dp[1], dp[0] + prev_score) + curr_score
+        dp[0], dp[1], dp[2] = dp[1], dp[2], next_dp
+        prev_score = curr_score
+        
+    print(dp[2])
