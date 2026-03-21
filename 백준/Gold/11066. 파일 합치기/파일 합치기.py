090d5523866df99d5f2c0b0ca6e72303ceff3ad4
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-21 20:14:21 +0900

    [Gold III] Title: 파일 합치기, Time: 1768 ms, Memory: 121844 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -5,10 +5,15 @@ for _ in range(T):
     lst = [*map(int, input().split())]
     DP = [[0 for _ in range(K)] for _ in range(K)]
 
+    pfs = [0] * (K + 1)
+    for i in range(K):
+        pfs[i + 1] = pfs[i] + lst[i]
+
     for n in range(2, K + 1):
         for i in range(K - n + 1):
-            mini = 999999999
+            mini = float("inf")
             for j in range(i, i + n - 1):
                 mini = min(mini, DP[i][j] + DP[j + 1][i + n - 1])
-            DP[i][i + n - 1] = mini + sum(lst[i : i + n])
+            DP[i][i + n - 1] = mini + (pfs[i + n] - pfs[i])
+
     print(DP[0][K - 1])
