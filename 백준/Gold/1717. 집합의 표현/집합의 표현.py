70b4fc67bcc8e506e93a9f5ad05e8abba22079c6
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-03-27 15:32:21 +0900

    [Gold V] Title: 집합의 표현, Time: 2896 ms, Memory: 120516 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,6 +1,5 @@
 n, m = map(int, input().split())
 parent = list(range(n + 1))
-size = [1] * (n + 1)
 
 def find(x):
     while parent[x] != x:
@@ -13,10 +12,7 @@ def union(a, b):
     rb = find(b)
     if ra == rb:
         return
-    if size[ra] < size[rb]:
-        ra, rb = rb, ra
     parent[rb] = ra
-    size[ra] += size[rb]
 
 for _ in range(m):
     op, a, b = map(int, input().split())
