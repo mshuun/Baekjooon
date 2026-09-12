4a1d0bca81aa25130fc6fa11f03538146c23afca
Sanghun Mo <106878967+mshuun@users.noreply.github.com>
2026-09-12 15:49:58 +0900

    [level 2] Title: H-Index, Time: 176.26 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,5 +1,4 @@
 def solution(arr):
-    arr.sort(reverse=1)
-    for i in range(arr[0],-1,-1):
+    for i in range(10000,-1,-1):
         if sum(1 for j in arr if j >= i) >= i :
             return i
